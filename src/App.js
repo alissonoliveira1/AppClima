92059121a4403137a5b425488d310bd72bb5d899
Alisson de Oliveira <72466422+alissonoliveira1@users.noreply.github.com>
2024-04-08 15:31:27 -0300

    in progress

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 function App() {
+
+  
+    const [weatherData, setWeatherData] = useState(null);
+
+    
+    
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const response = await axios.get('http://api.weatherapi.com/v1/current.json?', {
+            params: {
+              key: 'e24728ba2751422680c51701240704',
+              q: 'salvador',
+              region:'Bahia',
+              name:'salvador',
+              language:'pt'
+              
+            }
+          });
+          setWeatherData(response.data)
+         
+
+           
+      
+        } catch (error) {
+          console.error('Erro ao buscar dados de clima:', error);
+        }
+      };
+  
+      fetchData();
+    }, []);
+
+    function Tempo(){
+      if(weatherData.current.condition.code === 1009){
+        return(
+          <div>
+            
+          <div><img src={weatherData.current.condition.icon}/></div>  
+          <h3>{weatherData.current.condition.text}</h3>
+          </div>
+   
+        )
+      }
+    }
+    
+console.log(weatherData)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className='box'>   
+  
+     <div>
+      <h2>Informações de Clima</h2>
+      {weatherData && (
+        <div>
+          <p>Cidade: {weatherData.location.name}</p>
+          <h1>{weatherData.current.temp_c}°</h1>
+          
+          <Tempo/>
+
+        </div>
+      )}
     </div>
-  );
-}
+
+   
+    </div>
+    
+    </div>
+ 
+
+)}
 
 export default App;
